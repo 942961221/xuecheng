28cc14280eff868e1a5ae1b3567f9bf7290e08d8
zjw <942961221@qq.com>
2019-04-11 22:48:39 +0800

    添加一个测试类

---
+++ xc-service-manage-cms/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author : zhangJW
+ * @Description : java类作用描述
+ * @CreateDate : 2019/4/11 22:48
+ * @Updat :  更新作者
+ * @UpdateDate : 2019/4/11 22:48
+ * @UpdateRemark : 更新内容注释
+ * @Version : 1.0
+ */
+public class Test {
+}
